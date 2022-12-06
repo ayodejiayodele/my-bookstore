02b30c1264f444c6470b59ae0a664f9ddc5a6a2c
Ayò <63050478+ayodejiayodele@users.noreply.github.com>
2022-12-06 00:16:31 +0000

    Resort to basic javascript and ajax call

--- src/scripts/books.js
+++ src/scripts/books.js
@@ -3,25 +3,35 @@ function getBestsellerBooks() {
     var url = "https://www.amazon.com/gp/bestsellers/books/ref=zg_bs_nav_0";
     var books = [];
     var book = {};
-    var response = UrlFetchApp.fetch(url);
-    var html = response.getContentText();
-    var doc = HtmlService.createHtmlOutput(html);
-    var bookList = doc.$x("//div[@class='zg_itemImm");
+    //var response to get the html content of the url using xmlHttp
+    var ajax = XmlHttp.create();
+    ajax.open("GET", url);
+    ajax.send();
+    var response = ajax.getResponseText();
+    var html = response;
+    //get all div elements with class zg_itemImm from the html content
+    var bookList = html.match(/<div class="zg_itemImm">[\s\S]*?<\/div>/g);
+    var books = [];
+    var book = {};
+    //loop through the bookList to get the book details
     for (var i = 0; i < bookList.length; i++) {
-        var bookTitle = bookList[i].$x(".//div[@class='p13n-sc-truncate']");
-        var bookAuthor = bookList[i].$x(".//div[@class='a-row a-size-small']/a");
-        var bookPrice = bookList[i].$x(".//span[@class='p13n-sc-price']");
-        var bookImage = bookList[i].$x(".//img[@class='a-dynamic-image']");
-        var bookLink = bookList[i].$x(".//a[@class='a-link-normal']");
-
-        book = {
-            title: bookTitle[0].getText(),
-            author: bookAuthor[0].getText(),
-            price: bookPrice[0].getText(),
-            image: bookImage[0].getAttribute("src"),
-            link: bookLink[0].getAttribute("href")
-        };
+        //get the book title
+        book.title = bookList[i].match(/<div class="p13n-sc-truncate p13n-sc-line-clamp-1" dir="auto">[\s\S]*?<\/div>/g)[0].replace(/<div class="p13n-sc-truncate p13n-sc-line-clamp-1" dir="auto">/g, "").replace(/<\/div>/g, "");
+        //get the book author
+        book.author = bookList[i].match(/<div class="a-row a-size-small">[\s\S]*?<\/div>/g)[0].replace(/<div class="a-row a-size-small">/g, "").replace(/<\/div>/g, "");
+        //get the book image
+        book.image = bookList[i].match(/<img src="[\s\S]*?"/g)[0].replace(/<img src="/g, "").replace(/"/g, "");
+        //get the book price
+        book.price = bookList[i].match(/<span class="p13n-sc-price">[\s\S]*?<\/span>/g)[0].replace(/<span class="p13n-sc-price">/g, "").replace(/<\/span>/g, "");
+        //get the book rating
+        book.rating = bookList[i].match(/<span class="a-icon-alt">[\s\S]*?<\/span>/g)[0].replace(/<span class="a-icon-alt">/g, "").replace(/<\/span>/g, "");
+        //get the book url
+        book.url = bookList[i].match(/<a href="[\s\S]*?"/g)[0].replace(/<a href="/g, "").replace(/"/g, "");
+        //add the book to the books array
         books.push(book);
+        //reset the book object
+        book = {};
     }
+
     return books;
 }
